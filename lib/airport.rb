cfecfefdd9542c63987a6082f35d32e20314d078
Aiman Aziz Wahid <aiman15049@Aimans-MacBook-Air.local>
2018-12-10 03:31:54 +0000

    finished all stories pre refactor

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,31 @@
 
 class Airport
 
-def land(plane)
+  def initialize(capacity)
+    @capacity = 100
+    @planes = []
+    @weather_is_stormy = false
+  end
 
-end
+  def land(plane)
+    if @planes.length >= @capacity
+      raise "Cannot land plane, airport at full capacity"
+    elsif
+      @weather_is_stormy == true
+      raise "Cannot land plane, weather is too dangerous"
+    else
+      @planes << plane
+      puts "airplane landed"
+    end
+  end
 
-def take_off(plane)
-
+  def take_off(plane)
+    if @weather_is_stormy == true
+      raise "Cannot land plane, weather is too dangerous"
+    else
+      @planes.pop
+      puts "plane has taken off"
+  end
 end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,15 +1,24 @@
 require 'airport'
 
 describe Airport do
+  subject(:airport) { described_class.new(100) }
 
  it 'should instruct a plane to land' do
-   expect(subject).to respond_to(:land).with(1).argument
+   expect(airport).to respond_to(:land).with(1).argument
  end
+
 
  it 'should instruct a plane to take off' do
-   expect(subject).to respond_to(:take_off).with(1).argument
+   expect(airport).to respond_to(:take_off).with(1).argument
  end
 
+ it 'should not allow landing when at capacity' do
+   plane = double :plane
+   100.times do
+     airport.land(plane)
+   end
+   expect{ airport.land(plane) }.to raise_error "Cannot land plane, airport at full capacity"
+ end
 
 
 end
